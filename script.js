89b0a0b429a76f1b50e1521baff18f491cd7df01
piush bhandari <piushthegreat@piushs-MacBook-Pro.local>
2022-08-02 15:51:27 -0500

    ...

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ const ctaBtn = document.querySelector(".cards__cta");
 
 ctaBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  for (let x = 0; x <= 5; x++) {
-    window.open(
-      "https://www.youtube.com/watch?v=z0O32YA4Ibs&ab_channel=ThatCostumedGuy"
-    );
-  }
+
+  window.open(
+    "https://www.youtube.com/watch?v=z0O32YA4Ibs&ab_channel=ThatCostumedGuy"
+  );
 });
